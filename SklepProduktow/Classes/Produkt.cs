629a24d046e0c7589ad1c61f15523191d4efd40b
NicolaKaleta <NicolaKaleta@TE-LOD-D-04S109>
2025-12-12 12:08:54 +0100

    dodano konstruktory

---
+++ SklepProduktow/Classes/Produkt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepProduktow.Classes
+{
+    public class Produkt
+    {
+        private int _int;
+        private static int _licznik;
+        protected int _ilosc;
+        public string _nazwa;
+        public double _kwota;
+
+        public Produkt()
+        {
+            _nazwa = "Nieznane";
+            _kwota = 0.00;
+            _ilosc = 0;
+        }
+
+        public Produkt(string nazwa, double kwota)
+        {
+            _nazwa = nazwa;
+            _kwota = kwota;
+            _ilosc = 1;
+        }
+
+        public Produkt(string nazwa, double kwota, int ilosc)
+        {
+            _nazwa = nazwa;
+            _kwota = kwota;
+            _ilosc = ilosc;
+        }
+    }
+}
